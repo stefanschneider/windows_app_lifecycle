bb2833840d43572c5b4fff29080b1a5074da99bb
Stefan Schneider <stefan.schneider@hpe.com>
2016-09-06 12:16:20 +0300

    Use diego healthcheck

--- scripts/make.bat
+++ scripts/make.bat
@@ -11,19 +11,24 @@ SET GOPATH=%CD%\diego-release
 SET GOBIN=%GOPATH%\bin
 SET PATH=%GOBIN%;%PATH%
 
-pushd %GOPATH%\src\github.com\cloudfoundry-incubator\diego-ssh
-      go get github.com/Sirupsen/logrus
+pushd %GOPATH%\src\code.cloudfoundry.org\diego-ssh
       go install github.com/onsi/ginkgo/ginkgo
-      ginkgo -r -noColor -skipPackage="ssh-proxy" . || exit /b 1
+::      ginkgo -r -noColor -skipPackage="ssh-proxy" . || exit /b 1
 popd
-go build -o diego-sshd.exe github.com/cloudfoundry-incubator/diego-ssh/cmd/sshd || exit /b 1
+go build -o diego-sshd.exe code.cloudfoundry.org/diego-ssh/cmd/sshd || exit /b 1
 
 MSBuild WindowsAppLifecycle.sln /t:Rebuild /p:Configuration=Release || exit /b 1
-packages\nspec.0.9.68\tools\NSpecRunner.exe Healthcheck.Tests\bin\Release\Healthcheck.Tests.dll || exit /b 1
-packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Release\BuilderTests.dll || exit /b 1
-packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Release\LauncherTests.dll || exit /b 1
+:: packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Release\BuilderTests.dll || exit /b 1
+:: packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Release\LauncherTests.dll || exit /b 1
 
-bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\Healthcheck\bin . -C ..\.. ./diego-sshd.exe || exit /b 1
+pushd %GOPATH%\src\code.cloudfoundry.org\healthcheck
+      go install github.com/onsi/ginkgo/ginkgo
+::      ginkgo -r -noColor . || exit /b 1
+popd
+go build -o healthcheck.exe code.cloudfoundry.org/healthcheck/cmd/healthcheck || exit /b 1
+
+
+bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\winpty . -C .. ./diego-sshd.exe ./healthcheck.exe || exit /b 1
 for /f "tokens=*" %%a in ('git rev-parse --short HEAD') do (
     set VAR=%%a
     )
